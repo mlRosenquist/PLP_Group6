f37e0e08f28cdc72844e86fbd3a7b20b690c41a3
Morten Lyng Rosenquist <MLR@delegate.dk>
2021-03-25 11:23:39 +0100

    Updated coordinate to point

--- src/main/scala/models/Line.scala
+++ src/main/scala/models/Line.scala
@@ -1,13 +1,13 @@
 package main.scala.models
 
-class Line(_c1: Coordinate, _c2: Coordinate) extends Instruction {
+class Line(_c1: Point, _c2: Point) extends Instruction {
   var c1 = _c1
   var c2 = _c2
 }
 object Line {
   def parse(input: String): Line ={
     // TODO: IMPLEMENT PARSING HERE
-    new Line(new Coordinate(1,2), new Coordinate(3,4))
+    new Line(new Point(1,2), new Point(3,4))
   }
 
 }
--- src/main/scala/models/Point.scala
+++ src/main/scala/models/Point.scala
@@ -1,6 +1,6 @@
 package main.scala.models
 
-class Coordinate(_x: Int, _y: Int) {
+class Point(_x: Int, _y: Int) {
   var x = _x
   var y = _y
 
