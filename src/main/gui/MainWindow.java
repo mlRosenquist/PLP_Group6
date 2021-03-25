05fa20d959cd1cd639729c2178254127c946d480
Andrei Petriceanu <apetriceanu@live.com>
2021-03-25 08:51:09 +0100

    add gui

---
+++ src/main/gui/MainWindow.java
@@ -0,0 +1,16 @@
+package main.gui;
+
+import javax.swing.*;
+
+public class MainWindow {
+    private JPanel panel1;
+    private JTabbedPane tabbedPane1;
+    private JButton clearButton;
+    private JButton runCodeButton;
+    private JEditorPane codeEditor;
+    private JTextArea errorTextArea;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
