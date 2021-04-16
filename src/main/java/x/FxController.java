2feafc0ff507709d306c22b7e34b1737062db43f
Morten Lyng Rosenquist <MLR@delegate.dk>
2021-04-16 15:18:08 +0200

    Updated stuff

--- src/main/java/x/FxController.java
+++ src/main/java/x/FxController.java
@@ -1,11 +1,13 @@
 package main.java.x;
-import java.awt.*;
+import javafx.scene.control.*;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import main.scala.parser.Interpreter;
 
 
 public class FxController {
+    Interpreter interpreter;
     @FXML // ResourceBundle that was given to the FXMLLoader
     private ResourceBundle resources;
 
@@ -16,7 +18,7 @@ public class FxController {
     private TextArea logTextArea;
 
     @FXML
-    private TextArea codeTextArea;
+    private TextArea codeTxtArea;
 
     @FXML
     private Button runButton;
@@ -25,17 +27,26 @@ public class FxController {
     private Button clearButton;
 
     public FxController() {
-
+       interpreter = new Interpreter();
     }
 
     @FXML
     void initialize() {
 
+    // Prefilled with example
+    codeTxtArea.setText(
+                    "(LINE (7 8) (12 12))\n" +
+                    "(RECTANGLE (1 1) (2 2))\n" +
+                    "(CIRCLE (12 12) 3)\n" +
+                    "(TEXT-AT (1 1) Hello)\n" +
+                    "(BOUNDING-BOX (0 0) (16 12))\n" +
+                    "(DRAW green (LINE (7 8) (12 12)) (RECTANGLE (1 1) (2 2)) (CIRCLE (12 12) 3) (TEXT-AT (1 1) Hello) (BOUNDING-BOX (0 0) (16 12))\n" +
+                    "(FILL red (LINE (7 8) (12 12)))" );
     }
 
     @FXML
     private void console() {
-        System.out.println("Hello from FX!");
+        var instructions = interpreter.parse(codeTxtArea.getText());
     }
 
 }
