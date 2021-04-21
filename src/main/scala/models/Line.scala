0d68814766c7d7bb5ebccbc5bb8fd5f307fc1213
Andrei Petriceanu <apetriceanu@live.com>
2021-04-21 20:19:44 +0200

    Add Line algorithm

--- src/main/scala/models/Line.scala
+++ src/main/scala/models/Line.scala
@@ -1,17 +1,48 @@
 package main.scala.models
+import main.scala.RgbBitmap.RgbBitmap
 
-class Line(_start: Point, _end: Point) extends Figure {
-  var start = _start
-  var end = _end
+class Line(_start: Point, _end: Point, bm:RgbBitmap) extends Figure {
+  val start: Point = _start
+  val end: Point = _end
+
+  def draw(start: Point, end: Point): Iterator[(Int, Int)] = {
+    import scala.math.abs
+
+    val dx = abs(end.x - start.x)
+    val dy = abs(end.y - start.y)
+
+    val sx = if (start.x < end.x) 1 else -1
+    val sy = if (start.y < end.y) 1 else -1
+
+    new Iterator[(Int, Int)] {
+      var (x, y) = (start.x, start.y)
+      var err: Int = dx - dy
+
+      def next: (Int, Int) = {
+        val omitted = (x, y)
+        val e2 = 2 * err
+        if (e2 > -dy) {
+          err -= dy
+          x += sx
+        }
+        omitted
+      }
+
+      def hasNext: Boolean = sx*x <= sx*end.x && sy*y <= sy*end.y
+    }
+  }
+
+
 }
 object Line {
   def parse(input: String): Instruction ={
-    var splitInput = input.split(" ");
-    var start = Point.TryParse(splitInput(1), splitInput(2));
-    var end = Point.TryParse(splitInput(3), splitInput(4));
+    val splitInput = input.split(" ")
+    val start = Point.TryParse(splitInput(1), splitInput(2))
+    val end = Point.TryParse(splitInput(3), splitInput(4))
 
     (start, end) match {
-      case (p1: Point, p2: Point) => new Line(p1, p2);
+        //TODO: Reference the bounding box size for RgbBitmap
+      case (p1: Point, p2: Point) => new Line(p1, p2, new RgbBitmap(100,100));
       case (_, _) => new Error("Invalid Line: " + input);
     }
   }
