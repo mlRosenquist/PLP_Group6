a4016c67be9fdc22492fdc22d5af9c2e1fcfefc5
TobiasDJ <tobiasdammhenrichsen@gmail.com>
2021-05-01 13:43:20 +0200

    Added midpoint circle

--- src/main/scala/models/Circle.scala
+++ src/main/scala/models/Circle.scala
@@ -4,6 +4,7 @@ import javafx.scene.canvas.GraphicsContext
 import main.scala.drawer.CoordinateSystem
 import main.scala.parser.InstructionsEnum
 
+import java.awt.Color
 import java.awt.image.BufferedImage
 import scala.collection.mutable
 import scala.util.{Success, Try}
@@ -13,6 +14,42 @@ class Circle(_center: Point, _radius: Int) extends Figure {
   var radius = _radius;
 
   def draw(_coordinateSystem: CoordinateSystem): Unit ={
+    var centerPoint = _coordinateSystem.getPixelsFromCoordinate(center);
+    var circleRadius = (radius * _coordinateSystem.x_spacing)
+    drawMidpointCircle(centerPoint.x, centerPoint.y, circleRadius, _coordinateSystem)
+  }
+
+  def drawMidpointCircle(x0: Double, y0: Double, r: Double, coordinateSystem: CoordinateSystem): Unit ={
+    var x = r
+    var y = 0.0
+    var error = 0.0
+
+    while (x >= y){
+
+      drawPoints(x0.toInt, y0.toInt, x.toInt, y.toInt, coordinateSystem)
+
+      if(error <= 0){
+        y += 1
+        error += 2*y + 1
+      }
+      if (error > 0)
+      {
+        x -= 1;
+        error -= 2*x + 1;
+      }
+    }
+  }
+
+  def drawPoints(x0: Int, y0: Int, x: Int, y: Int, coordinateSystem: CoordinateSystem): Unit ={
+    coordinateSystem.drawPixel(x0 + x, y0 + y, Color.BLACK)
+    coordinateSystem.drawPixel(x0 + y, y0 + x, Color.BLACK)
+    coordinateSystem.drawPixel(x0 - y, y0 + x, Color.BLACK)
+    coordinateSystem.drawPixel(x0 - x, y0 + y, Color.BLACK)
+    coordinateSystem.drawPixel(x0 - x, y0 - y, Color.BLACK)
+    coordinateSystem.drawPixel(x0 - y, y0 - x, Color.BLACK)
+    coordinateSystem.drawPixel(x0 + y, y0 - x, Color.BLACK)
+    coordinateSystem.drawPixel(x0 + x, y0 - y, Color.BLACK)
+
   }
 }
 object Circle {
