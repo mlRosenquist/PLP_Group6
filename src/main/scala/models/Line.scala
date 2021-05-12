d245b25372fd9c5757e0a2f67a094d3fcb24f0f2
Andrei Petriceanu <apetriceanu@live.com>
2021-05-12 16:57:08 +0200

    Working line, but messed up scaling

--- src/main/scala/models/Line.scala
+++ src/main/scala/models/Line.scala
@@ -1,6 +1,5 @@
 package main.scala.models
-import main.scala.RgbBitmap.RgbBitmap
-import main.scala.drawer.CoordinateSystem
+import scala.math.abs
 
 import scala.collection.mutable.ArrayBuffer
 
@@ -11,7 +10,54 @@ class Line(_start: Point, _end: Point) extends Figure {
    def draw(_boundingBox: BoundingBox): ArrayBuffer[Point] = {
      var pixels = new ArrayBuffer[Point]();
 
-     return pixels;
+     val dx = math.abs(end.x - start.x) * _boundingBox.xSpacing
+     val dy = math.abs(end.y - start.y) * _boundingBox.ySpacing
+
+     val sx = if (start.x < end.x) 1 else -1
+     val sy = if (start.y < end.y) 1 else -1
+
+     def it: Iterator[(Double, Double)] = new Iterator[(Double, Double)] {
+       var x: Double = start.x
+       var y: Double = start.y
+       var err: Double = (if (dx > dy) dx else -dy)/2
+
+       def next: (Double, Double) = {
+         val res = (x, y)
+         val e2 = err
+         if (e2 > -dx) {
+           err -=dy
+           x += sx
+         }
+
+         if (e2 < dy) {
+           err += dx
+           y += sy
+         }
+         res
+       }
+
+       def hasNext: Boolean = (sx*x <= sx*end.x && sy*y <= sy*end.y)
+     }
+
+     for ((x,y) <- it) {
+       //pixels.addOne(new Point(x, y, this.color))
+       pixels = pixels.concat(drawPoints(start.x.toInt, start.y.toInt, x.toInt, y.toInt))
+     }
+
+     pixels
+  }
+
+  def drawPoints(x0: Int, y0: Int, x: Int, y: Int): ArrayBuffer[Point] ={
+    val pixels = new ArrayBuffer[Point]()
+    pixels.addOne(new Point(x0 + x, y0 + y, this.color));
+    pixels.addOne(new Point(x0 + y, y0 + x, this.color));
+    pixels.addOne(new Point(x0 - y, y0 + x, this.color));
+    pixels.addOne(new Point(x0 - x, y0 + y, this.color));
+    pixels.addOne(new Point(x0 - x, y0 - y, this.color));
+    pixels.addOne(new Point(x0 - y, y0 - x, this.color));
+    pixels.addOne(new Point(x0 + y, y0 - x, this.color) );
+    pixels.addOne(new Point(x0 + x, y0 - y, this.color));
+    return pixels;
   }
 }
 
